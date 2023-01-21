d716326bafed4d02434c2e3fc87427c41729c828
Talhaitis612 <bsem-f18-206@superior.edu.pk>
2023-01-22 00:24:09 +0500

    forms integrated

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,6 +8,8 @@ import {Subject} from 'rxjs';
 @Injectable()
 export class RecipeService {
 
+  recipesChanged = new Subject<Recipe[]>();
+
   private recipes: Recipe[] = [
     new Recipe(
       'Tasty Schnitzel',
@@ -37,5 +39,19 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+  addRecipe(recipe : Recipe){
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes);
+  }
+
+  updateRecipe(index : number, newRecipe : Recipe){
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+
+  }
+  deleteRecipe(index : number){
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -7,7 +7,6 @@ import { Subscription } from 'rxjs';
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
-  providers: [RecipeService],
 })
 export class RecipesComponent implements OnInit {
   constructor() {}
